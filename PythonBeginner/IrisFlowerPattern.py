ea9454137ffbb868716490d0f5c81d11b1b1cd57
Maithreyi M <maithreyi85@gmail.com>
2019-01-26 16:03:33 -0600

    ExerciseComplete-IrisPrediction

--- PythonBeginner/IrisFlowerPattern.py
+++ PythonBeginner/IrisFlowerPattern.py
@@ -6,7 +6,6 @@
 # will “learn from the data” and extract patterns from it. 
 #It will then apply what it learned to a new dataset. 
 
-
 #importing the necessary libraries from sklearn.datasets import load_iris
 from sklearn.datasets import load_iris
 from sklearn import tree
@@ -14,18 +13,20 @@ from sklearn.metrics import accuracy_score
 import numpy as np
  #loading the iris dataset
 iris = load_iris()
-print(iris)
 x = iris.data #array of the data
 y = iris.target #array of labels (i.e answers) of each data entry
  #getting label names i.e the three flower species
 y_names = iris.target_names
  #taking random indices to split the dataset into train and test
 test_ids = np.random.permutation(len(x))
- #splitting data and labels into train and test
+#splitting data and labels into train and test
 #keeping last 10 entries for testing, rest for training
 x_train = x[test_ids[:-10]]
+#print (len(x_train))
 x_test = x[test_ids[-10:]]
+#print(len(x_test))
 y_train = y[test_ids[:-10]]
+#print(len(y_train))
 y_test = y[test_ids[-10:]]
  #classifying using decision tree
 clf = tree.DecisionTreeClassifier()
@@ -33,5 +34,9 @@ clf = tree.DecisionTreeClassifier()
 clf.fit(x_train, y_train)
  #predictions on the test dataset
 pred = clf.predict(x_test)
+print(pred)
+print(y_test)
+print((accuracy_score(pred, y_test))*100) #prediction accuracy
+
 
  
